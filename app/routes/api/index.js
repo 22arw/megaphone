af81cc6873ef0a8d8decb3aad5ecd3b339823757
Kevin Falting <kevinfalting@gmail.com>
2018-10-07 11:42:57 -0500

    set up router pattern

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const registerRouter = require('./register');
+
 router
   .get('/', (req, res, next) => {
     res.send('This is the base api route.');
     // TODO: Eventually serve an api reference page at the root
   })
-  .get('/register', (req, res, next) => {
-    res.send(`Sent from ${req.url}`);
-  });
+  .use('/register', registerRouter);
 
 module.exports = router;
