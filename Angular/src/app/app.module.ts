680777dc31053ed9bdcf3dd530dd225dc8a7803f
Kevin Falting <kevinfalting@gmail.com>
2018-10-09 11:44:18 -0500

    add home, landing, navbar, and register components

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
