e334e4ef3d69f8deaf2e8f4a3214dd9b371763fc
Kevin Falting <kevinfalting@gmail.com>
2018-10-19 16:17:08 -0500

    add calls to get the organizations the user is a part of.

--- app/controllers/dbInterfaces/getAllDataAssociatedWithUser.js
+++ app/controllers/dbInterfaces/getAllDataAssociatedWithUser.js
@@ -1,4 +1,7 @@
+const Sequelize = require('sequelize');
 const models = require('../../db/models');
+
+const Op = Sequelize.Op;
 
 const getAllDataAssociatedWithUser = async userId => {
   // Collect data from User table
@@ -25,22 +28,46 @@ const getAllDataAssociatedWithUser = async userId => {
     console.error(err);
   });
 
-  // Collect data from Base table that the user manages
-  baseIds = baseManagerData.map(base => {
+  // Collect data from OrganizationManager table
+  const organizationManagerData = await models.OrganizationManager.findAll({
+    where: {
+      userId: userId
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+
+  // Collect data from Organization table that the user manages
+  const organizationManagerIds = organizationManagerData.map(org => {
+    return Number(org.orgId);
+  });
+
+  const organizationData = await models.Organization.findAll({
+    where: {
+      id: organizationManagerIds
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+
+  // Collect data from what Bases these organizations are a part of AND
+  // Collect data from Base table that the user manages or has organizations that they are a part of
+  const organizationsBaseIds = organizationData.map(org => {
+    return Number(org.baseId);
+  });
+  const baseIds = baseManagerData.map(base => {
     return Number(base.baseId);
   });
 
   const baseData = await models.Base.findAll({
     where: {
-      id: baseIds
+      id: [...new Set(baseIds.concat(organizationsBaseIds))]
     }
   }).catch(err => {
     console.error(err);
   });
 
   // TODO:
-  // Collect data from OrganizationManager table
-  // Collect data from Organization table that the user manages
   // Collect the number of subscribers for each organization the user manages
   // Collect all of the messages for each organization that the user manages. This is really a low priority. They didn't ask for this functionality and it's going to require requerying all the tables again.
   // Format the data so it's much easier to read on the front end.
@@ -48,7 +75,9 @@ const getAllDataAssociatedWithUser = async userId => {
   const result = {
     userData: userData,
     baseManagerData: baseManagerData,
-    baseData: baseData
+    baseData: baseData,
+    organizationManagerData: organizationManagerData,
+    organizationData: organizationData
   };
 
   return result;
