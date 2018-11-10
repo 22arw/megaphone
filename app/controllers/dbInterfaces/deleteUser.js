974ab40f5c6ec9c9d1de3c897a2452e63e4d1d65
Kevin Falting <kevinfalting@gmail.com>
2018-11-10 13:15:46 -0600

    add commented build instructions to delete user

--- app/controllers/dbInterfaces/deleteUser.js
+++ app/controllers/dbInterfaces/deleteUser.js
@@ -3,4 +3,8 @@
  * Delete user by id only
  *
  * - Removing a user has a lot of consequences and needs to be thoughtfully executed. If we just remove the user from the database, then all of their references will be broken in other tables.
+ *
+ * - Set isActive = false
+ * - Remove all of their records from basemanager and orgmanager tables
+ * - how to handle if orgOwner? Make them transfer ownership before removing account? - This may be the best way to ensure proper handoff.
  */
