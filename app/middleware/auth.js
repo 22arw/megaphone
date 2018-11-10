37dab5de21fc671ea3877323be6724b8e750f309
Kevin Falting <kevinfalting@gmail.com>
2018-11-09 19:54:54 -0600

    a

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,7 +5,9 @@ module.exports = (req, res, next) => {
   let token =
     req.body.token || req.query.token || req.headers['x-access-token'];
   console.log('TIME: ', new Date().toLocaleString());
-  console.log(token);
+  console.log(`TOKEN BODY: ${req.body.token}`);
+  console.log(`TOKEN QUERY: ${req.query.token}`);
+  console.log(`TOKEN HEADER: ${req.headers['x-access-token']}`);
 
   if (req.path == '/auth/login') {
     return next();
