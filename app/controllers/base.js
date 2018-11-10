805ed4ddfe6f8215fa18ff300e534e9942b1fe8b
Kevin Falting <kevinfalting@gmail.com>
2018-11-09 19:25:12 -0600

    a

--- app/controllers/base.js
+++ app/controllers/base.js
@@ -4,7 +4,7 @@ const utils = require('../utils');
 
 module.exports = {
   createBase: async (req, res) => {
-    console.log(JSON.stringify(req.body));
+    console.log(req.body);
     const basePhoneNumber = _.toString(req.body.basePhoneNumber).trim();
     const baseName = _.toString(req.body.baseName).trim();
     const bandwidthUserId = _.toString(req.body.bandwidthUserId).trim();
