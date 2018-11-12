aff54ebceaf55b4858ade9e4002ca25de9b05b7f
Kevin Falting <kevinfalting@gmail.com>
2018-11-12 10:42:36 -0600

    a

--- app/middleware/keywordHandler.js
+++ app/middleware/keywordHandler.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 
 module.exports = async (req, res, next) => {
   console.log(`\nMessage came in: `);
-  console.log(req);
+  console.log(JSON.stringify(req));
   const text = _.toString(req.body.text).trim().toLowerCase();
   const phoneNumber = _.toString(req.body.from).trim();
   const basePhoneNumber = _.toString(req.body.to).trim();
