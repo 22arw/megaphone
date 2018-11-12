907a344cd84ec51c8ed7683b4b67cebee6d57d7d
Kevin Falting <kevinfalting@gmail.com>
2018-11-12 11:05:18 -0600

    a

--- app/middleware/keywordHandler.js
+++ app/middleware/keywordHandler.js
@@ -4,7 +4,9 @@ const util = require('util');
 
 module.exports = async (req, res, next) => {
   console.log(`\nMessage came in: `);
-  console.log(util.inspect(req, false, 1, true));
+  console.log(`Host: ${req.headers.host}`);
+  console.log(`Origin: ${req.headers.origin}`);
+
   const text = _.toString(req.body.text).trim().toLowerCase();
   const phoneNumber = _.toString(req.body.from).trim();
   const basePhoneNumber = _.toString(req.body.to).trim();
