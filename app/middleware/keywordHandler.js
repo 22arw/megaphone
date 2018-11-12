70a9b5eed3c5281394ebaf68fe3d9025ef78a655
Kevin Falting <kevinfalting@gmail.com>
2018-11-12 11:15:03 -0600

    a

--- app/middleware/keywordHandler.js
+++ app/middleware/keywordHandler.js
@@ -1,11 +1,11 @@
 const dbInterface = require('../controllers/dbInterfaces');
 const _ = require('lodash');
-const util = require('util');
+const {inspect} = require('util');
 
 module.exports = async (req, res, next) => {
-  console.log(`\nMessage came in: `);
-  console.log(`Headers: ${req.headers}\n`);
-  console.log(`Body: ${req.body}\n`);
+  console.log(`\nMessage came in:\n`);
+  console.log(`Headers: ${inspect(req.headers)}\n`);
+  console.log(`Body: ${inspect(req.body)}\n`);
 
   const text = _.toString(req.body.text).trim().toLowerCase();
   const phoneNumber = _.toString(req.body.from).trim();
