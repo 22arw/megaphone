9a695a38eab72a59e359bb885608394286d316ab
Kevin Falting <kevinfalting@gmail.com>
2018-11-19 14:44:28 -0600

    a

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,10 +5,10 @@ module.exports = (req, res, next) => {
   process.stdout.write('Attempting to authenticate... ');
   let token =
     req.body.token || req.query.token || req.headers['x-access-token'];
-  // console.log('TIME: ', new Date().toLocaleString());
-  // console.log(`TOKEN BODY: ${req.body.token}`);
-  // console.log(`TOKEN QUERY: ${req.query.token}`);
-  // console.log(`TOKEN HEADER: ${req.headers['x-access-token']}`);
+  console.log('TIME: ', new Date().toLocaleString());
+  console.log(`TOKEN BODY: ${req.body.token}`);
+  console.log(`TOKEN QUERY: ${req.query.token}`);
+  console.log(`TOKEN HEADER: ${req.headers['x-access-token']}`);
 
   if (req.path == '/auth/login') {
     return next();
@@ -23,7 +23,6 @@ module.exports = (req, res, next) => {
         }
       });
     } else {
-      console.log('\n');
       console.log('Token not valid');
       return res.sendStatus(401);
     }
