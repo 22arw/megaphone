cd554237eefd2a5f4aeb26d08f0f2e91fb91dce6
Kevin Falting <kevinfalting@gmail.com>
2018-11-28 14:03:53 -0600

    a

--- app/controllers/dbInterfaces/doesOrgExistBySubscriptionCode.js
+++ app/controllers/dbInterfaces/doesOrgExistBySubscriptionCode.js
@@ -1,11 +1,14 @@
 const models = require('../../db/models');
 
 module.exports = async subscriptionCode => {
+  console.log('subscriptionCode:', subscriptionCode);
   const org = await models.Organization.findAll({
     where: {
       subscriptionCode: subscriptionCode
     }
-  }).catch(err => console.error(err));
+  });
+
+  console.log('org:', org)
 
   return Array.isArray(org) && org.length === 1;
 };
